69d49f1f6b5108b83ff8600e3dc1e18d8fa59c21
jaiv shah <jaiv.maurya@gmail.com>
2020-09-23 22:42:01 -0400

    now added all files using 'git add.' command

---
+++ app/src/main/java/com/example/lab1/AI_Activity.java
@@ -0,0 +1,14 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AI_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_i_);
+    }
+}
